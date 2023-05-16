654c0e2d0b28fc96dd5bbcfeaca36f8fe07df098
MuxauL-cpu <neverno128@mail.ru>
2023-05-16 19:09:05 +0300

    Добавил обработку падения сервера

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors);
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
